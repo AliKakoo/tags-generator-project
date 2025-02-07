6a13f15287343294e1cd6bd0bf9cd639e44cd004
AliKakoo <alikakuo@yahoo.com>
2025-02-07 20:30:04 +0330

    add new feature for tags number

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// tags = ["Sabzlearn", "JS"];
+const $ = document;
+
+const tagsUlElem = $.querySelector("ul");
+const tagInputElem = $.querySelector("input");
+const tagsCountSpanElem = $.querySelector("span");
+const removeAllBtn = $.querySelector("button");
+
+let tags = ["sabzlearn", "js" , "React"];
+let maxTagsCount = 10;
+
+const countTags = () => {
+  tagInputElem.focus();
+  tagsCountSpanElem.innerText = maxTagsCount - tags.length;
+};
+
+const removeTag = (tagElem) => {
+  console.log(tagElem);
+};
+
+const createTag = () => {
+  let tagli = null;
+
+  tags.forEach((tag) => {
+    tagli = `<li>${tag}<i class = " uit uit-multiply" onclick="removeTag(this)"></i></li>`;
+    tagsUlElem.insertAdjacentHTML('afterbegin', tagli);
+  });
+};
+
+createTag()
+countTags();
