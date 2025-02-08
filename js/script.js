3a0768808eb60bb04252f5f2a98b811ebeba6f40
AliKakoo <alikakuo@yahoo.com>
2025-02-08 21:57:46 +0330

    add final feature

--- js/script.js
+++ js/script.js
@@ -5,48 +5,55 @@ const tagInputElem = $.querySelector("input");
 const tagsCountSpanElem = $.querySelector("span");
 const removeAllBtn = $.querySelector("button");
 
-let tags = ["sabzlearn", "js", "React"];
+let tags = [];
 let maxTagsCount = 10;
 
 const removeAllLis = () => {
- tagsUlElem.querySelectorAll("li").forEach(tag => tag.remove())
-}
+  tagsUlElem.querySelectorAll("li").forEach((tag) => tag.remove());
+};
 
 const countTags = () => {
   tagInputElem.focus();
   tagsCountSpanElem.innerText = maxTagsCount - tags.length;
 };
 
-const removeTag = (tagElem) => {
-  console.log(tagElem);
+const removeTag = (tagElem , tagTitle) => {
+
+  let mainTagIndex = tags.indexOf(tagTitle);
+  tags.splice(mainTagIndex, 1);
+  tagElem.parentElement.remove();
+  countTags();
 };
 
 const createTag = () => {
-
-  removeAllLis()
+  removeAllLis();
 
   let tagli = null;
 
   [...tags].reverse().forEach((tag) => {
-    tagli = `<li>${tag}<i class = " uit uit-multiply" onclick="removeTag(this)"></i></li>`;
+    tagli = `<li>${tag}<i class = " uit uit-multiply" onclick="removeTag(this ,'${tag}')"></i></li>`;
     tagsUlElem.insertAdjacentHTML("afterbegin", tagli);
   });
 
-  countTags()
+  countTags();
 };
 
 const addTag = (event) => {
   if (event.key === "Enter") {
     let tagTitle = event.target.value;
-    console.log(tagTitle)
+    console.log(tagTitle);
 
-    if (tags.length < 10) {
-      if (tagTitle.length < 10 && tagTitle.length > 0) {
-        tags.push(tagTitle)
-        createTag()
-      }
+    if (tags.length < 10 && !tags.includes(tagTitle.toLowerCase())) {
+        tagTitle.split(',').forEach(tag => {
+
+          tags.push(tag.toLowerCase());
+        })
+      
+    }else {
+      alert("Duplicate value!!!");
     }
-    event.target.value = ''
+    event.target.value = "";
+    createTag();
   }
 };
 
@@ -54,10 +61,9 @@ createTag();
 countTags();
 
 tagInputElem.addEventListener("keyup", addTag);
-removeAllBtn.addEventListener('click', ()=>{
-  tags.length = 0
+removeAllBtn.addEventListener("click", () => {
+  tags.length = 0;
 
-  removeAllLis()
+  removeAllLis();
   countTags();
-
-})
+});
