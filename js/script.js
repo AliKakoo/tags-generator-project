f2031f75715c746401e854d34d33c095856f9b06
AliKakoo <alikakuo@yahoo.com>
2025-02-08 07:41:47 +0330

    add new featur for add new tag in box

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ const tagInputElem = $.querySelector("input");
 const tagsCountSpanElem = $.querySelector("span");
 const removeAllBtn = $.querySelector("button");
 
-let tags = ["sabzlearn", "js" , "React"];
+let tags = ["sabzlearn", "js", "React"];
 let maxTagsCount = 10;
+
+const removeAllLis = () => {
+ tagsUlElem.querySelectorAll("li").forEach(tag => tag.remove())
+}
 
 const countTags = () => {
   tagInputElem.focus();
@@ -18,13 +22,42 @@ const removeTag = (tagElem) => {
 };
 
 const createTag = () => {
+
+  removeAllLis()
+
   let tagli = null;
 
-  tags.forEach((tag) => {
+  [...tags].reverse().forEach((tag) => {
     tagli = `<li>${tag}<i class = " uit uit-multiply" onclick="removeTag(this)"></i></li>`;
-    tagsUlElem.insertAdjacentHTML('afterbegin', tagli);
+    tagsUlElem.insertAdjacentHTML("afterbegin", tagli);
   });
+
+  countTags()
 };
 
-createTag()
+const addTag = (event) => {
+  if (event.key === "Enter") {
+    let tagTitle = event.target.value;
+    console.log(tagTitle)
+
+    if (tags.length < 10) {
+      if (tagTitle.length < 10 && tagTitle.length > 0) {
+        tags.push(tagTitle)
+        createTag()
+      }
+    }
+    event.target.value = ''
+  }
+};
+
+createTag();
 countTags();
+
+tagInputElem.addEventListener("keyup", addTag);
+removeAllBtn.addEventListener('click', ()=>{
+  tags.length = 0
+
+  removeAllLis()
+  countTags();
+
+})
